fafea366a33166ada833ac1f642f05ea30d341f8
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-08-23 15:17:26 +0000

    Setup basic agavi unit and flow test integration. refs #4097

---
+++ testing/run-tests.php
@@ -0,0 +1,38 @@
+<?php
+// +---------------------------------------------------------------------------+
+// | Initialize some common directory vars and set our include path.           |
+// +---------------------------------------------------------------------------+
+$root_dir = dirname(dirname(__FILE__));
+$base_dir = dirname(__FILE__);
+$libs_dir = $root_dir . DIRECTORY_SEPARATOR . 'libs';
+
+$includes = array($libs_dir, $libs_dir . DIRECTORY_SEPARATOR . 'PHPUnit');
+set_include_path(implode(PATH_SEPARATOR, $includes).PATH_SEPARATOR.get_include_path());
+
+// +---------------------------------------------------------------------------+
+// | An absolute filesystem path to our environment config provider.           |
+// +---------------------------------------------------------------------------+
+require $root_dir . DIRECTORY_SEPARATOR . 'app/lib/config/ProjectEnvironmentConfig.class.php';
+
+// +---------------------------------------------------------------------------+
+// | An absolute filesystem path to the agavi/testing.php script.              |
+// +---------------------------------------------------------------------------+
+require 'agavi/testing.php';
+
+// +---------------------------------------------------------------------------+
+// | An absolute filesystem path to our config.php script.                     |
+// +---------------------------------------------------------------------------+
+require $base_dir . DIRECTORY_SEPARATOR . 'config.php';
+
+AgaviToolkit::clearCache();
+
+$env =  'testing.'.ProjectEnvironmentConfig::getEnvironment();
+AgaviTesting::bootstrap($env);
+
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(AgaviConfig::get('core.agavi_dir'));
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(AgaviConfig::get('core.cache_dir'));
+
+$arguments = AgaviTesting::processCommandlineOptions(); 
+AgaviTesting::dispatch($arguments);
+
+?>
