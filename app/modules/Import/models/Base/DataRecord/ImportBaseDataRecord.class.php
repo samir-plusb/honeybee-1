e7d5057dbfb86ee35e3cda9b5a78e72c56617cf5
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-01 12:46:14 +0000

    The ImportBaseDataRecord now implements the IComparable interface. refs #4237

--- app/modules/Import/models/Base/DataRecord/ImportBaseDataRecord.class.php
+++ app/modules/Import/models/Base/DataRecord/ImportBaseDataRecord.class.php
@@ -1,31 +1,49 @@
 <?php
 
-abstract class ImportBaseDataRecord implements IDataRecord
+abstract class ImportBaseDataRecord implements IDataRecord, IComparable
 {
     protected $data;
-    
+
     abstract protected function parse($dataSrc);
-    
+
     public function __construct($dataSrc)
     {
         $this->data = $this->parse($dataSrc);
     }
-    
+
     public function getSupportedFields()
     {
         return array_keys($this->data);
     }
-    
+
     public function getValue($fieldname, $default = null)
     {
         $value = $default;
-        
+
         if (isset($this->data[$fieldname]))
         {
             $value = $this->data[$fieldname];
         }
-        
+
         return $value;
+    }
+
+    public function compareTo($other)
+    {
+        if (!($other instanceof IDataRecord))
+        {
+            throw new DataRecordException("Unable to compare non-datarecord type with data-record.");
+        }
+
+        foreach ($this->getSupportedFields() as $supportedField)
+        {
+            if ($this->getValue($supportedField) !== $other->getValue($supportedField))
+            {
+                return -1;
+            }
+        }
+
+        return 0;
     }
 }
 
