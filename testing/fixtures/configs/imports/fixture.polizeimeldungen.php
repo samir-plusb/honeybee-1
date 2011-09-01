b760dedc76646949ab50472b7738971955d4d4d1
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-01 09:58:43 +0000

    Repaired fixtures that where affected as the imperia polizeimeldungs document fixture was introduced inside the fixture data directory. refs #4239

--- testing/fixtures/configs/imports/fixture.polizeimeldungen.php
+++ testing/fixtures/configs/imports/fixture.polizeimeldungen.php
@@ -1,29 +1,24 @@
 <?php
 
-return array (
-  'import' => 
-  array (
-    'class' => 'ImperiaDataImport',
-    'name' => 'Polizeimeldungen',
-    'description' => 'Imports the imperia "Polizeimeldungen" data into our couch database.',
-    'settings' => 
-    array (
-      'couch_host' => 'localhost',
-      'couch_port' => '5984',
+return array(
+    'import' => array(
+        'class' => 'ImperiaDataImport',
+        'name' => 'Polizeimeldungen',
+        'description' => 'Imports the imperia "Polizeimeldungen" data into our couch database.',
+        'settings' => array(
+            'couch_host' => 'localhost',
+            'couch_port' => '5984',
+        ),
+        'datasource' => array(
+            'class' => 'ImperiaXmlDataSource',
+            'name' => 'Polizeimeldungen',
+            'description' => 'Provides access to the imperia "Polizeimeldungen" data via http.',
+            'settings' => array(
+                'url' => 'https://imperia.berlinonline.de/',
+                'account_user' => 'hans',
+                'account_pass' => 'wurst',
+            ),
+        ),
     ),
-    'datasource' => 
-    array (
-      'class' => 'ImperiaXmlDataSource',
-      'name' => 'Polizeimeldungen',
-      'description' => 'Provides access to the imperia "Polizeimeldungen" data via http.',
-      'settings' => 
-      array (
-        'url' => 'http://imperia.berlinonline.de',
-        'account_user' => 'hans',
-        'account_pass' => 'wurst',
-      ),
-    ),
-  ),
-)
-
+    )
 ?>
