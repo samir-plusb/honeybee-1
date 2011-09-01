fd24ec214d7f3a9b5d519ba3d353645919848901
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-01 12:45:29 +0000

    Finished with implementing the DataSource tests and removed unneeded fixture. refs #4239

--- testing/fixtures/data/polizeimeldungen.datasource.php
+++ testing/fixtures/data/polizeimeldungen.datasource.php
@@ -4,6 +4,11 @@ return array(
     'url' => 'https://imperia.berlinonline.de',
     'account_user' => 'shrink',
     'account_pass' => 'th0rst3n',
-    'doc_ids'      => array('/2/10330/10343/10890/1385807')  // normally these are provided by the imperia-trigger script.
+    'doc_ids'      => array( // normally these are provided by the imperia-trigger script.
+        '/2/10330/10343/10890/1385807',
+        '/2/10330/10343/10890/1385806',
+        '/2/10330/10343/10890/1385805'
+    )
 );
+
 ?>
--- testing/tests/unit/modules/Import/models/DataSource/ImperiaDataSourceTest.php
+++ testing/tests/unit/modules/Import/models/DataSource/ImperiaDataSourceTest.php
@@ -2,7 +2,11 @@
 
 class ImperiaDataSourceTest extends AgaviUnitTestCase
 {
-    const CFG_FIXTURE = 'data/polizeimeldungen.datasource.php';
+    const CFG_CONFIG_FIXTURE = 'data/polizeimeldungen.datasource.php';
+
+    const CFG_XML_FIXTURE = 'data/polizeimeldung.article.xml';
+
+    const RECORDS_ARE_EQUAL = 0;
 
     protected $imperiaDataSource;
 
@@ -17,18 +21,47 @@ class ImperiaDataSourceTest extends AgaviUnitTestCase
         );
     }
 
-    public function testNextRecord()
+    public function testNextRecordIsValid()
     {
         $record = $this->imperiaDataSource->nextRecord();
+
+        $expectedRecord = new ImperiaDataRecord(
+            $this->loadXmlArticleFixture()
+        );
+
+        $this->assertEquals(self::RECORDS_ARE_EQUAL, $record->compareTo($expectedRecord));
+    }
+
+    public function testNextRecordLoop()
+    {
+        $expectedCount = 3;
+        $currentCount = 0;
+        $record = null;
+
+        while (($record = $this->imperiaDataSource->nextRecord()))
+        {
+            $currentCount++;
+        }
+
+        $this->assertEquals($expectedCount, $currentCount);
     }
 
     protected function loadConfigFixture()
     {
         $baseDir = AgaviConfig::get('core.testing_dir') . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR;
 
-        $fixtureFile = $baseDir . self::CFG_FIXTURE;
+        $fixtureFile = $baseDir . self::CFG_CONFIG_FIXTURE;
 
         return include $fixtureFile;
+    }
+
+    protected function loadXmlArticleFixture()
+    {
+        $baseDir = AgaviConfig::get('core.testing_dir') . DIRECTORY_SEPARATOR . 'fixtures' . DIRECTORY_SEPARATOR;
+
+        $fixtureFile = $baseDir . self::CFG_XML_FIXTURE;
+
+        return file_get_contents($fixtureFile);
     }
 }
 
