6e0d03e4db801de8dd994157cc9d3e60b96abd82
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-01 17:14:38 +0000

    Renamed vars inside the configure-env script to be more meaningful. refs #4097

--- bin/configure-env.php
+++ bin/configure-env.php
@@ -4,12 +4,12 @@
 //#------------------------------------ DIRECTORIES & INCLUDES ----------------------------------------------#
 //#----------------------------------------------------------------------------------------------------------#
 
-$base_dir = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
+$testing_dir = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
 $lib_conf_dir = 'app' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
 
-$environment_config_class_file = $base_dir . $lib_conf_dir . 'ProjectEnvironmentConfig.class.php';
-$configurator_class_file = $base_dir . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'configure' . DIRECTORY_SEPARATOR . 'EnvironmentConfigurator.class.php';
-$configure_script_class_file = $base_dir . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'configure' . DIRECTORY_SEPARATOR . 'ConfigureEnvScript.class.php';
+$environment_config_class_file = $testing_dir . $lib_conf_dir . 'ProjectEnvironmentConfig.class.php';
+$configurator_class_file = $testing_dir . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'configure' . DIRECTORY_SEPARATOR . 'EnvironmentConfigurator.class.php';
+$configure_script_class_file = $testing_dir . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'configure' . DIRECTORY_SEPARATOR . 'ConfigureEnvScript.class.php';
 
 require_once $environment_config_class_file;
 require_once $configurator_class_file;
