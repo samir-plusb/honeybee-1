5097dd6f0fe754469f7a8f907be1736f30d2b1ee
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-02 13:43:27 +0000

    Started to add main entry point for the import package. refs #4237

--- app/modules/Import/impl/Imperia/ImperiaAction.class.php
+++ app/modules/Import/impl/Imperia/ImperiaAction.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * @copyright BerlinOnline
@@ -7,23 +8,33 @@
  */
 class Import_ImperiaAction extends ImportBaseAction
 {
+    const PARAM_CONFIG_FILE = 'import_config';
 
-
-	/**
-	 * Handles the Write request method.
-	 *
-	 * @parameter  AgaviRequestDataHolder the (validated) request data
-	 *
-	 * @return     mixed <ul>
-	 *                     <li>A string containing the view name associated
-	 *                     with this action; or</li>
-	 *                     <li>An array with two indices: the parent module
-	 *                     of the view to be executed and the view to be
-	 *                     executed.</li>
-	 *                   </ul>^
-	 */
 	public function executeWrite(AgaviRequestDataHolder $rd)
 	{
+        $configFile = $rd->getParameter(self::PARAM_CONFIG_FILE);
+        $config = new ImperiaImportFactoryConfig($configFile);
+
+        $importFactory = new DataImportFactory($config);
+
+        $import = $importFactory->createImport('ImperiaDataImportConfig');
+        $dataSource = $importFactory->createDataSource('ImperiaDataSourceConfig');
+
+        if (!$import->run($dataSource))
+        {
+            
+        }
+
 		return 'Success';
 	}
+
+    private function getImportConfigDirectory()
+    {
+        $baseDir = AgaviConfig::get('core.config_dir') . DIRECTORY_SEPARATOR . 'imports' . DIRECTORY_SEPARATOR;
+
+        return $baseDir . self::PARAM_CONFIG_FILE;
+
+    }
 }
+
+?>
