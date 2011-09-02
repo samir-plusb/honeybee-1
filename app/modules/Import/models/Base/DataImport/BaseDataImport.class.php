0c9efb3d535357d414f8c26748d6f176f1dfe6a9
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-02 08:41:06 +0000

    Whitespace comitt, replaced tabs by spaces. refs #4237

--- app/modules/Import/models/Base/DataImport/BaseDataImport.class.php
+++ app/modules/Import/models/Base/DataImport/BaseDataImport.class.php
@@ -3,44 +3,46 @@
 abstract class BaseDataImport implements IDataImport
 {
     private $dataSource;
-    
+
     protected $config;
-    
+
     protected abstract function importRecord(IDataRecord $record);
 
     public function __construct(IImportConfig $config)
     {
         $this->config = $config;
     }
-    
+
     public function run(IDataSource $dataSource)
     {
         $this->dataSource = $dataSource;
-        
+
         $this->init();
-        
+
         while ($record = $dataSource->nextRecord())
         {
             $this->importRecord($record);
         }
-        
+
         $this->cleanup();
-        
+
         $this->dataSource = null;
+
+        return true;
     }
-    
+
     protected function getDataSource()
     {
         if (null === $this->dataSource)
         {
             throw new DataImportException("The datasource member is only available inside the run method's execution scope.");
         }
-        
+
         return $this->dataSource;
     }
-    
+
     protected function init() {}
-    
+
     protected function cleanup() {}
 }
 
