a45de11008963c9eddc4503510daeaab12203dc4
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-07 16:30:16 +0000

    Added tests for the ProjectEventProxy class. refs #4239

---
+++ testing/tests/unit/models/Event/ProjectEventProxyTest.php
@@ -0,0 +1,72 @@
+<?php
+
+class ProjectEventProxyTest extends AgaviPhpUnitTestCase
+{
+    const EVENT_TEST_PROXY = 'midas.testing.events.test_proxy';
+    
+    private $eventProxy;
+    
+    private $eventReceived;
+    
+    private $eventCount;
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->eventProxy = ProjectEventProxy::getInstance();
+        $this->eventReceived = FALSE;
+        $this->eventCount = 0;
+    }
+    
+    public function testSubscribe()
+    {
+        $this->eventProxy->subscribe(
+            self::EVENT_TEST_PROXY,
+            array($this, 'eventCallbackMock')
+        );
+        
+        $this->eventProxy->publish(
+            new ProjectEvent(self::EVENT_TEST_PROXY)
+        );
+        
+        $this->eventProxy->publish(
+            new ProjectEvent(self::EVENT_TEST_PROXY)
+        );
+        
+        $this->assertEquals(TRUE, $this->eventReceived);
+        $this->assertEquals(2, $this->eventCount);
+    }
+    
+    public function testUnsubscribe()
+    {
+        $this->eventProxy->subscribe(
+            self::EVENT_TEST_PROXY,
+            array($this, 'eventCallbackMock')
+        );
+        
+        $this->eventProxy->publish(
+            new ProjectEvent(self::EVENT_TEST_PROXY)
+        );
+        
+        $this->eventProxy->unsubscribe(
+            self::EVENT_TEST_PROXY,
+            array($this, 'eventCallbackMock')
+        );
+        
+        $this->eventProxy->publish(
+            new ProjectEvent(self::EVENT_TEST_PROXY)
+        );
+        
+        $this->assertEquals(TRUE, $this->eventReceived);
+        $this->assertEquals(1, $this->eventCount);
+    }
+    
+    public function eventCallbackMock(IEvent $event)
+    {
+        $this->eventReceived = TRUE;
+        $this->eventCount++;
+    }
+}
+
+?>
