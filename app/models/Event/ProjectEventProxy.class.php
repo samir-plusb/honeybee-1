97c99e92c81f27b6e49bdd0a51cd39e10f2436aa
aeytom <tom.anheyer@berlinonline.de>
2011-09-08 08:09:16 +0000

    ProjectEventProxy.class.php: increase fault tolerance of publish(); refs #4101

--- app/models/Event/ProjectEventProxy.class.php
+++ app/models/Event/ProjectEventProxy.class.php
@@ -13,114 +13,117 @@
 class ProjectEventProxy implements IEventProxy
 {
     // ---------------------------------- <MEMBERS> ----------------------------------------------
-    
+
     /**
      * An array containing our current event subscriptions.
-     * 
+     *
      * @var         array
      */
     private $subscriptions = array();
-    
+
     /**
      * Holds a static instance of this class.
-     * 
+     *
      * @var         ProjectEventProxy
      */
     private static $instance;
-    
+
     // ---------------------------------- </MEMBERS> ---------------------------------------------
-    
-    
+
+
     // ---------------------------------- <IEventProxy IMPL> -------------------------------------
-    
+
     /**
      * Subscribe to an event by providing the desired event name
      * and a callback that shall be invoked when the event is published.
-     * 
+     *
      * @param       string $eventName An event name such as 'midas.events.import.record_success'
      * @param       array $callback An array containing an object and callable public method name.
-     * 
+     *
      * @see         IEventProxy::subscribe()
      */
     public function subscribe($eventName, array $callback)
     {
         $this->checkCallback($callback);
-        
+
         if (!isset($this->subscriptions[$eventName]))
         {
             $this->subscriptions[$eventName] = array();
         }
-        
+
         $this->subscriptions[$eventName][] = $callback;
     }
-    
+
     /**
      * Unsubscribe from an event by providing the desired event name
      * and a callback that shall be invoked when the event is published.
-     * 
+     *
      * @param       string $eventName An event name such as 'midas.events.import.record_success'
      * @param       array $callback An array containing an object and callable public method name.
-     * 
+     *
      * @see         IEventProxy::unsubscribe()
      */
     public function unsubscribe($eventName, array $callback)
     {
         $this->checkCallback($callback);
-        
+
         if (!isset($this->subscriptions[$eventName]))
         {
             return;
         }
-        
+
         list($object, $methodName) = $callback;
-        
+
         $callbackIndex = -1;
         $curIndex = 0;
-        
+
         foreach ($this->subscriptions[$eventName] as $curCallback)
         {
             list($curObject, $curMethodName) = $curCallback;
-            
+
             if ($curObject === $object && $curMethodName === $methodName)
             {
                 $callbackIndex = $curIndex;
                 break;
             }
-            
+
             $curIndex++;
         }
-        
+
         array_splice($this->subscriptions[$eventName], $callbackIndex, 1);
     }
-    
+
     /**
      * Publish the given event to all registered subscribers.
-     * 
+     *
      * @param       IEvent $event
-     * 
+     *
      * @see         IEventProxy::publish()
      */
     public function publish(IEvent $event)
     {
-        foreach ($this->subscriptions[$event->getName()] as $curCallback)
+        if (isset($this->subscriptions[$event->getName()]) && is_array($this->subscriptions[$event->getName()]))
         {
-            list($curObject, $curMethodName) = $curCallback;
-            $curObject->$curMethodName($event);
+            foreach ($this->subscriptions[$event->getName()] as $curCallback)
+            {
+                list($curObject, $curMethodName) = $curCallback;
+                $curObject->$curMethodName($event);
+            }
         }
     }
-    
+
     // ---------------------------------- </IEventProxy IMPL> ------------------------------------
-    
-    
+
+
     // ---------------------------------- <PUBLIC METHODS> ---------------------------------------
-    
+
     /**
      * Returns a static instance of this class,
      * as a workaround to easily making it available for everyone around,
      * without needing to do any stunts.
      * Yes we know global state is bad, but this is the so called occasion.
      * If you find any other global state around please file a bug.
-     * 
+     *
      * @return      ProjectEventProxy
      */
     public static function getInstance()
@@ -129,20 +132,20 @@ class ProjectEventProxy implements IEventProxy
         {
             self::$instance = new ProjectEventProxy();
         }
-        
+
         return self::$instance;
     }
-    
+
     // ---------------------------------- </PUBLIC METHODS> --------------------------------------
-    
-    
+
+
     // ---------------------------------- <WORKING METHODS> --------------------------------------
-    
+
     /**
      * Checks if the given callback is callable.
-     * 
-     * @param       array $callback 
-     * 
+     *
+     * @param       array $callback
+     *
      * @throws      Exception If the provided $callback is not callable.
      */
     protected function checkCallback(array $callback)
@@ -152,7 +155,7 @@ class ProjectEventProxy implements IEventProxy
             throw new Exception("Non executeable callback provided.");
         }
     }
-    
+
     // ---------------------------------- </WORKING METHODS> -------------------------------------
 }
 
