a138cc893ed531869ec0dc578e02185d7b49ced8
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-09 09:45:09 +0000

    Removed the json output-type from the imperia action's views as it is not required atm. refs #4237

--- app/modules/Import/impl/Imperia/ImperiaErrorView.class.php
+++ app/modules/Import/impl/Imperia/ImperiaErrorView.class.php
@@ -12,33 +12,6 @@
 class Import_Imperia_ImperiaErrorView extends ImportBaseView
 {
     /**
-     * Handles the Json output type.
-     *
-     * @parameter  AgaviRequestDataHolder the (validated) request data
-     *
-     * @return     mixed <ul>
-     *                     <li>An AgaviExecutionContainer to forward the execution to or</li>
-     *                     <li>Any other type will be set as the response content.</li>
-     *                   </ul>
-     */
-    public function executeJson(AgaviRequestDataHolder $parameters)
-    {
-        $this->setupJson($parameters);
-        $errors = array();
-        
-        foreach ($this->getContainer()->getValidationManager()->getErrorMessages() as $error)
-        {
-            $errors[] = $error['message'];
-        }
-        
-        $this->getResponse()->setContent(
-            json_decode(
-                array('state' => 'error', 'errors' => $errors)
-            )
-        );
-    }
-
-    /**
      * Handles the Console output type.
      *
      * @parameter  AgaviRequestDataHolder the (validated) request data
--- app/modules/Import/impl/Imperia/ImperiaSuccessView.class.php
+++ app/modules/Import/impl/Imperia/ImperiaSuccessView.class.php
@@ -12,23 +12,6 @@
 class Import_Imperia_ImperiaSuccessView extends ImportBaseView
 {
     /**
-     * Handles the Json output type.
-     *
-     * @parameter  AgaviRequestDataHolder the (validated) request data
-     *
-     * @return     mixed <ul>
-     *                     <li>An AgaviExecutionContainer to forward the execution to or</li>
-     *                     <li>Any other type will be set as the response content.</li>
-     *                   </ul>
-     */
-    public function executeJson(AgaviRequestDataHolder $parameters)
-    {
-        $this->setupJson($parameters);
-        $data = array('state' => 'success');
-        $this->getResponse()->setContent(json_encode($data));
-    }
-
-    /**
      * Handles the Console output type.
      *
      * @parameter  AgaviRequestDataHolder the (validated) request data
