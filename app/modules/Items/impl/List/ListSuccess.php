5636985a821ef43e6f887a02201c9f9ae34e298f
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-16 10:47:45 +0000

    Added the item id hidden inside a rows class to the list success. refs #4111

--- app/modules/Items/impl/List/ListSuccess.php
+++ app/modules/Items/impl/List/ListSuccess.php
@@ -4,9 +4,9 @@
 
 foreach ($t['items'] as $item)
 {
-;
+    $class = 'item_' . $item['id'];
 ?>
-    <li>
+    <li class="<?php echo $class; ?>">
         <h3><?php echo $item['title']; ?></h3>
         <p>
             <?php echo $item['content']; ?>
