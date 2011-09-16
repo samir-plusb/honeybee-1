43bfe89111aab62e620b79e5d2870b93b71179a4
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-09-16 11:06:20 +0000

    Removed unused class from the ListSuccess template. refs #4111

--- app/modules/Items/impl/List/ListSuccess.php
+++ app/modules/Items/impl/List/ListSuccess.php
@@ -1,23 +1,20 @@
 <ul>
 
 <?php
-
 foreach ($t['items'] as $item)
 {
-    $class = 'item_' . $item['_id'];
-
     echo '<!-- '.print_r($item,1).' -->';
 ?>
-    <li class="<?php echo $class; ?>">
+
+    <li>
         <h3><?php echo $item['title']; ?></h3>
         <p>
             <?php echo $item['content']; ?>
         </p>
     </li>
+
 <?php
-
 }
-
 ?>
 
 </ul>
