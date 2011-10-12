0a3d6fcfade6cae5825d79289de62a898a6b7120
aeytom <tom.anheyer@berlinonline.de>
2011-10-12 06:52:33 +0000

    refine error handling and startup/dispatch phase; fix errors with testing environment; refs #4527

--- app/dispatch.php
+++ app/dispatch.php
@@ -11,8 +11,6 @@ $phpUnitDir = $libsDir . DIRECTORY_SEPARATOR . 'PHPUnit';
 $includes = array($libsDir, $ezComponentsDir, $phpUnitDir);
 set_include_path(implode(PATH_SEPARATOR, $includes).PATH_SEPARATOR.get_include_path());
 
-$testingEnabled = false;
-
 // make generated files group writeable for easy switch between web/console
 umask(02);
 
@@ -24,12 +22,11 @@ require $rootDir . DIRECTORY_SEPARATOR . '/app/config.php';
 // +---------------------------------------------------------------------------+
 require $ezComponentsDir . DIRECTORY_SEPARATOR . 'Base/src/ezc_bootstrap.php';
 spl_autoload_register(array('ezcBase', 'autoload'));
-
 // +---------------------------------------------------------------------------+
 // | An absolute filesystem path to our environment config provider.           |
 // +---------------------------------------------------------------------------+
 require $rootDir . DIRECTORY_SEPARATOR . 'app/lib/config/ProjectEnvironmentConfig.class.php';
-ProjectEnvironmentConfig::load($testingEnabled);
+ProjectEnvironmentConfig::load(isset($testingEnabled));
 
 
 // +---------------------------------------------------------------------------+
--- pub/index.php
+++ pub/index.php
@@ -1,33 +1,20 @@
 <?php
-// +---------------------------------------------------------------------------+
-// | Require our dispatch script, that takes care loading libs and environment.|
-// +---------------------------------------------------------------------------+
-$testingEnabled = false;
+
 $rootDir = dirname(dirname(__FILE__));
 require $rootDir . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'dispatch.php';
 
 // +---------------------------------------------------------------------------+
-// | An absolute filesystem path to the agavi/agavi.php script.                |
-// +---------------------------------------------------------------------------+
-require 'agavi/agavi.php';
-
-// +---------------------------------------------------------------------------+
-// | An absolute filesystem path to our app/config.php script.                 |
-// +---------------------------------------------------------------------------+
-require $rootDir . DIRECTORY_SEPARATOR . 'app/config.php';
-
-// +---------------------------------------------------------------------------+
 // | PhpDebugToolbar integration                                               |
 // +---------------------------------------------------------------------------+
-// 
+//
 // @todo Think about this, is the right include condition?
-if (!strstr(ProjectEnvironmentConfig::getEnvironment(), 'live'))
+if (! strstr(ProjectEnvironmentConfig::getEnvironment(), 'live'))
 {
-    $debugbarDir = 
-        $rootDir . DIRECTORY_SEPARATOR . 
-        'libs' . DIRECTORY_SEPARATOR . 
+    $debugbarDir =
+        $rootDir . DIRECTORY_SEPARATOR .
+        'libs' . DIRECTORY_SEPARATOR .
         'PhpDebugToolbar' . DIRECTORY_SEPARATOR;
-    
+
     require $debugbarDir . 'PhpDebugToolbar.class.php';
 
     PhpDebugToolbar::start(array(
@@ -36,29 +23,12 @@ if (!strstr(ProjectEnvironmentConfig::getEnvironment(), 'live'))
     ));
 }
 
-// +---------------------------------------------------------------------------+
-// | Initialize the framework. You may pass an environment name to this method.|
-// | By default the 'development' environment sets Agavi into a debug mode.    |
-// | In debug mode among other things the cache is cleaned on every request.   |
-// +---------------------------------------------------------------------------+
-
-// @todo Atm this is needed to support routes that rely on the $_SERVER var for their source attribute.
-$_SERVER['AGAVI_ENVIRONMENT'] = ProjectEnvironmentConfig::toEnvString();
-
-Agavi::bootstrap(
-    ProjectEnvironmentConfig::toEnvString()
+AgaviConfig::set(
+    'core.default_context',
+    preg_match('/\/binaries/i', $_SERVER['QUERY_STRING'])
+        ? 'web_binaries'
+        : 'web'
 );
-
-// +---------------------------------------------------------------------------+
-// | Call the controller's dispatch method on the default context              |
-// +---------------------------------------------------------------------------+
-if (preg_match('/\/binaries/i', $_SERVER['QUERY_STRING']))
-{
-    AgaviContext::getInstance('web_binaries')->getController()->dispatch();
-}
-else
-{
-    AgaviContext::getInstance('web')->getController()->dispatch();
-}
+AgaviContext::getInstance()->getController()->dispatch();
 
 ?>
