46f6df6131461abb0bf31673c6c5e0a8c1dfca7b
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-10-17 15:00:11 +0000

    Introduced two new containers for splitting the 'editing form' (left column) into two subcolumns editing::left and editing::right. refs #4533

--- app/modules/Items/impl/Edit/EditSuccess.php
+++ app/modules/Items/impl/Edit/EditSuccess.php
@@ -11,19 +11,17 @@
     </aside>
 </header>
 
-<menu class="action-button-list">
-    <li>
-        <a href="#list">Liste</a>
-        <span class="info-small">1</span>
-    </li>
-    <li><a href="#store">Speichern</a></li>
-    <li><a href="#new">Neues Item</a></li>
-    <li><a href="#delete">L&ouml;schen</a></li>
-    <li><a href="#complete">Abhaken</a></li>
-</menu>
-
 <section class="document-editing">
     <h2>Document editing form</h2>
+    <menu class="action-button-list">
+        <li>
+            <a href="#list">Liste</a>
+            <span class="info-small">1</span>
+        </li>
+        <li><a href="#store">Speichern</a></li>
+        <li><a href="#new">Neues Item</a></li>
+        <li><a href="#delete">L&ouml;schen</a></li>
+    </menu>
     <form accept-charset="utf-8" action="#postdata" method="post">
         <input type="hidden" name="data[status]" id="status" />
 
@@ -79,94 +77,90 @@
             </div>
         </fieldset>
 
-        <fieldset class="geo-data content-panel">
-            <legend>Geo</legend>
-            <input type="hidden" name="data[location[longitude]]" id="location[longitude]" />
-            <input type="hidden" name="data[location[latitude]]" id="location[latitude]" />
-            <div class="input-full">
-                <select name="data[location[relevance]]" id="location[relevance]">
-                    <option value="" selected="selected"></option>
-                    <option value="0" selected="selected">Betrifft den Bezirk (z.B. Wilmersdorf)</option>
-                    <option value="1">Betrifft den Verwaltungsbezirk (z.B. Charlottenburg-Wilmersdorf)</option>
-                    <option value="2">Betrifft die ganze Stadt</option>
-                </select>
-            </div>
-            <div class="input-full">
-                <label for="location[name]">Name des Orts (z.B: KaDeWe)</label>
-                <input name="data[location[name]]" type="text" id="location[name]" />
-            </div>
-            <div class="input-full"
-                 ><label for="location[locationdetail]">Zusätzliche Ortsangabe (z.B.: Haus 3)</label>
-                <input name="data[location[locationdetail]]" type="text" id="location[locationdetail]" />
-            </div>
-            <div class="input-full">
-                <label for="location[street]">Straße, Hausnummer</label>
-                <input name="data[location[street]]" type="text" id="location[street]" />
-            </div>
-            <div class="input-full">
-                <label for="location[uzip]">PLZ</label>
-                <input name="data[location[uzip]]" type="text" id="location[uzip]" />
-            </div>
-            <div class="input-full">
-                <label for="location[neighborhood]">Bezirk</label>
-                <input name="data[location[neighborhood]]" type="text" disabled="disabled" id="location[neighborhood]" />
-            </div>
-            <div class="input-full">
-                <label for="location[subneighborhood]">Alter Bezirksname</label>
-                <input name="data[location[subneighborhood]]" type="text" disabled="disabled" id="location[subneighborhood]" />
-            </div>
-        </fieldset>
+        <div class="extra-data-left">
+            <fieldset class="geo-data content-panel">
+                <legend>Geo</legend>
+                <input type="hidden" name="data[location[longitude]]" id="location[longitude]" />
+                <input type="hidden" name="data[location[latitude]]" id="location[latitude]" />
+                <div class="input-full">
+                    <select name="data[location[relevance]]" id="location[relevance]">
+                        <option value="" selected="selected"></option>
+                        <option value="0" selected="selected">Betrifft den Bezirk (z.B. Wilmersdorf)</option>
+                        <option value="1">Betrifft den Verwaltungsbezirk (z.B. Charlottenburg-Wilmersdorf)</option>
+                        <option value="2">Betrifft die ganze Stadt</option>
+                    </select>
+                </div>
+                <div class="input-full">
+                    <label for="location[name]">Name des Orts (z.B: KaDeWe)</label>
+                    <input name="data[location[name]]" type="text" id="location[name]" />
+                </div>
+                <div class="input-full"
+                     ><label for="location[locationdetail]">Zusätzliche Ortsangabe (z.B.: Haus 3)</label>
+                    <input name="data[location[locationdetail]]" type="text" id="location[locationdetail]" />
+                </div>
+                <div class="input-full">
+                    <label for="location[street]">Straße, Hausnummer</label>
+                    <input name="data[location[street]]" type="text" id="location[street]" />
+                </div>
+                <div class="input-full">
+                    <label for="location[uzip]">PLZ</label>
+                    <input name="data[location[uzip]]" type="text" id="location[uzip]" />
+                </div>
+                <div class="input-full">
+                    <label for="location[neighborhood]">Bezirk</label>
+                    <input name="data[location[neighborhood]]" type="text" disabled="disabled" id="location[neighborhood]" />
+                </div>
+                <div class="input-full">
+                    <label for="location[subneighborhood]">Alter Bezirksname</label>
+                    <input name="data[location[subneighborhood]]" type="text" disabled="disabled" id="location[subneighborhood]" />
+                </div>
+            </fieldset>
+        </div>
+        <div class="extra-data-right">
+            <fieldset class="datetime-data content-panel">
+                <legend>Zeiten</legend>
+                <div class="input-full">
+                    <label for="date[isevent]">Ist Teilnahme des Nutzers durch den Veranstalter erwünscht?</label>
+                    <input type="hidden" name="data[date[isevent]]" id="date[isevent]_" value="0" />
+                    <input type="checkbox" name="data[date[isevent]]" value="1" id="date[isevent]" />
+                </div>
+                <div class="input-full">
+                    <label for="date[from]">
+                        Wann findet das statt? <span>Bei Ausstellungen: Startdatum = Enddatum</span>. von
+                    </label>
+                    <input name="data[date[from]]" type="text" id="date[from]" />
+                </div>
+                <div class="input-full">
+                    <label for="date[till]">bis</label>
+                    <input name="data[date[till]]" type="text" id="date[till]" />
+                </div>
+            </fieldset>
 
-        <fieldset class="datetime-data content-panel">
-            <legend>Zeiten</legend>
-            <div class="input checkbox">
-                <label for="date[isevent]">Ist Teilnahme des Nutzers durch den Veranstalter erwünscht?</label>
-                <input type="hidden" name="data[date[isevent]]" id="date[isevent]_" value="0" />
-                <input type="checkbox" name="data[date[isevent]]" value="1" id="date[isevent]" />
-            </div>
-            <div>
-                <label for="date[from]">
-                    Wann findet das statt? <span>Bei Ausstellungen: Startdatum = Enddatum</span>. von
-                </label>
-                <input name="data[date[from]]" type="text" id="date[from]" />
-            </div>
-            <div>
-                <label for="date[till]">bis</label>
-                <input name="data[date[till]]" type="text" id="date[till]" />
-            </div>
-        </fieldset>
-
-        <section class="nearby-items content-panel">
-            <h3>Items in der Nähe</h3>
-            <ul>
-                <li data-template="">
-                    <a href="/items/edit/{{Item.parent}}">
-                        ({{Item.location.neighborhood|truncate 100}}) {{Item.title|truncate 80}}
-                    </a>
-                </li>
-            </ul>
-        </section>
+            <section class="nearby-items content-panel">
+                <h3 class="legend">Items in der Nähe</h3>
+                <ul>
+                    <li data-template="">
+                        <a href="/items/edit/{{Item.parent}}">
+                            ({{Item.location.neighborhood|truncate 100}}) {{Item.title|truncate 80}}
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </div>
     </form>
 </section>
 
 <section class="document-data">
-    <section class="item-pool content-panel">
-        <h2>Items</h2>
-        <ul>
-            <li data-template="">
-                <article>
-                    <hgroup>
-                        <h3>{{title|truncate 45}}</h3>
-                    </hgroup>
-                    <p>{{date.from}}</p>
-                    <p>{{text|truncate 100}}</p>
-                </article>
-            </li>
-        </ul>
-    </section>
+    <h2>Current Import Item Data</h2>
+    <menu class="action-button-list">
+        <li><a href="#store">Zur&uuml;ck</a></li>
+        <li><a href="#new">Abhaken</a></li>
+        <li><a href="#delete">L&ouml;schen</a></li>
+        <li><a href="#complete">Vor</a></li>
+    </menu>
 
-    <section class="item-meta-data content-panel">
-        <h2>Meta Daten</h2>
+    <div class="item-meta-data content-panel">
+        <h3 class="legend">Daten</h3>
         <dl>
             <dt>Betreff</dt>
             <dd></dd>
@@ -175,10 +169,10 @@
             <dt>Versanddatum</dt>
             <dd>01:00 - 01.01.1970</dd>
         </dl>
-    </section>
+    </div>
 
-    <section class="item-content content-panel">
-        <h2>Content</h2>
+    <div class="item-content content-panel">
+        <h3 class="legend">Inhalt</h3>
         <ul>
             <li>
                 <a href="#content-tab-1">Inhalt</a>
@@ -187,5 +181,22 @@
         <div id="content-tab-1">
             <textarea id="textinner" readonly="readonly" class="textinner"></textarea>
         </div>
-    </section>
+    </div>
 </section>
+
+<!-- @todo
+<section class="content-items">
+    <h3>Items</h3>
+    <ul>
+        <li data-template="">
+            <article>
+                <hgroup>
+                    <h3>{{title|truncate 45}}</h3>
+                </hgroup>
+                <p>{{date.from}}</p>
+                <p>{{text|truncate 100}}</p>
+            </article>
+        </li>
+    </ul>
+</section>
+-->
