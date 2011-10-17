67ad10192613b124038ba02451084e78c76a159c
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-10-17 08:59:05 +0000

    Finshed markup refactoring for the content worker editing frontend. refs #4533

--- app/modules/Items/impl/Edit/EditSuccess.php
+++ app/modules/Items/impl/Edit/EditSuccess.php
@@ -1,46 +1,35 @@
-<!-- ######################## document header ######################## -->
 <header>
     <h1>
-        <a href="#"><?php echo $t['_title']; ?></a>
+        <a href="#home"><?php echo $t['_title']; ?></a>
     </h1>
-    <section id="completed">
+    <aside class="personal-info">
         <h2>Userinfo Box</h2>
         <p>
-            Diese Nachricht wird von <a href="/items/reportPerson/"></a> bearbeitet.
+            Diese Nachricht wird von <a href="#profile?"></a> bearbeitet.
             Du hast bisher 0 Mails abgehakt.
         </p>
-    </section>
+    </aside>
 </header>
 
-<!-- ######################## document navigation ######################## -->
-<nav class="itemControls">
-    <h2>Main Navigation</h2>
-    <ul>
-        <li class="button">
-            <a href="" id="action_list">Liste</a>
-            <span id="itemscounter">0</span>
-        </li>
-        <li class="button red"><a href="" id="action_save">Speichern</a></li>
-        <li class="button red"><a href="" id="action_new">Neues Item</a></li>
-        <li class="button"><a href="" id="action_delete">Löschen</a></li>
-        <li class="button right"></li>
-        <li class="button">
-            <a href="#" id="action_complete">Abhaken</a>
-        </li>
-        <li class="button"></li>
-        <li class="button"></li>
-    </ul>
-</nav>
+<menu class="action-button-list">
+    <li>
+        <a href="#list">Liste</a>
+        <span class="info-small">1</span>
+    </li>
+    <li><a href="#store">Speichern</a></li>
+    <li><a href="#new">Neues Item</a></li>
+    <li><a href="#delete">L&ouml;schen</a></li>
+    <li><a href="#complete">Abhaken</a></li>
+</menu>
 
-<!-- ######################## document editing ######################## -->
-<section id="content" class="items panel2">
-    <h2 class="legend">Redaktionelle Einstellungen</h2>
-    <form id="itemsEditForm" accept-charset="utf-8" action="/items/edit/" method="post">
+<section class="document-editing">
+    <h2>Document editing form</h2>
+    <form accept-charset="utf-8" action="#postdata" method="post">
         <input type="hidden" name="data[status]" id="status" />
 
-        <fieldset>
-            <legend class="legend">Hauptdaten</legend>
-            <div class="vertical">
+        <fieldset class="content-panel">
+            <legend>Redaktionelle Einstellungen</legend>
+            <div class="input-left category">
                 <label for="Kategorie">Kategorie</label>
                 <select name="data[category]" id="category">
                     <option value=""></option>
@@ -51,7 +40,7 @@
                     <option value="Bekanntmachung">Bekanntmachung</option>
                 </select>
             </div>
-            <div class="vertical">
+            <div class="input-left priority">
                 <label for="Priorität">Priorität</label>
                 <select name="data[priority]" id="priority">
                     <option value=""></option>
@@ -60,41 +49,41 @@
                     <option value="2">hoch</option>
                 </select>
             </div>
-            <div class="vertical">
+            <div class="input-left editor">
                 <label for="username">Bearbeiter:</label>
                 <input name="data[username]" type="text" disabled="disabled" class="username" id="username" />
             </div>
-            <div class="input text title_box">
+            <div class="input-full title">
                 <label for="title">Titel</label>
                 <input name="data[title]" type="text" id="title" />
             </div>
-            <div class="input textarea">
+            <div class="input-full">
                 <label for="teaser">
                     Teaser. Die ersten drei Sätze Deines Texts, die Du selber schreiben kannst.
                 </label>
                 <textarea name="data[teaser]" cols="2" rows="6" id="teaser" ></textarea>
             </div>
-            <div class="input textarea">
+            <div class="input-full">
                 <label for="text">
                     <strong>Text</strong>. Der Rest des Texts. Hier sollst Du vor allem kürzen.
                 </label>
                 <textarea name="data[text]" rows="10" cols="30" id="text" ></textarea>
             </div>
-            <div class="input text">
+            <div class="input-full">
                 <label for="source">Quelle (Wer hat das geschickt. Z.B.: Bezirksamt Marzahn-Hellersdorf. Unbedingt ausfüllen.)</label>
                 <input name="data[source]" type="text" id="source" />
             </div>
-            <div class="input text">
+            <div class="input-full">
                 <label for="url">URL (Verknüpfte Internetadresse)</label>
                 <input name="data[url]" type="text" id="url" />
             </div>
         </fieldset>
 
-        <fieldset class="half1">
-            <legend class="legend">Geo</legend>
+        <fieldset class="geo-data content-panel">
+            <legend>Geo</legend>
             <input type="hidden" name="data[location[longitude]]" id="location[longitude]" />
             <input type="hidden" name="data[location[latitude]]" id="location[latitude]" />
-            <div class="input text">
+            <div class="input-full">
                 <select name="data[location[relevance]]" id="location[relevance]">
                     <option value="" selected="selected"></option>
                     <option value="0" selected="selected">Betrifft den Bezirk (z.B. Wilmersdorf)</option>
@@ -102,53 +91,56 @@
                     <option value="2">Betrifft die ganze Stadt</option>
                 </select>
             </div>
-            <div class="input text">
+            <div class="input-full">
                 <label for="location[name]">Name des Orts (z.B: KaDeWe)</label>
                 <input name="data[location[name]]" type="text" id="location[name]" />
             </div>
-            <div class="input text"><label for="location[locationdetail]">Zusätzliche Ortsangabe (z.B.: Haus 3)</label>
+            <div class="input-full"
+                 ><label for="location[locationdetail]">Zusätzliche Ortsangabe (z.B.: Haus 3)</label>
                 <input name="data[location[locationdetail]]" type="text" id="location[locationdetail]" />
             </div>
-            <div class="input text">
+            <div class="input-full">
                 <label for="location[street]">Straße, Hausnummer</label>
                 <input name="data[location[street]]" type="text" id="location[street]" />
             </div>
-            <div class="input text">
+            <div class="input-full">
                 <label for="location[uzip]">PLZ</label>
                 <input name="data[location[uzip]]" type="text" id="location[uzip]" />
             </div>
-            <div class="input text">
+            <div class="input-full">
                 <label for="location[neighborhood]">Bezirk</label>
                 <input name="data[location[neighborhood]]" type="text" disabled="disabled" id="location[neighborhood]" />
             </div>
-            <div class="input text">
+            <div class="input-full">
                 <label for="location[subneighborhood]">Alter Bezirksname</label>
                 <input name="data[location[subneighborhood]]" type="text" disabled="disabled" id="location[subneighborhood]" />
             </div>
         </fieldset>
 
-        <fieldset class="half2">
-            <legend class="legend">Zeiten</legend>
+        <fieldset class="datetime-data content-panel">
+            <legend>Zeiten</legend>
             <div class="input checkbox">
                 <label for="date[isevent]">Ist Teilnahme des Nutzers durch den Veranstalter erwünscht?</label>
                 <input type="hidden" name="data[date[isevent]]" id="date[isevent]_" value="0" />
                 <input type="checkbox" name="data[date[isevent]]" value="1" id="date[isevent]" />
             </div>
-            <div class="input text">
-                <label for="date[from]">Wann findet das statt? <strong>Bei Ausstellungen: Startdatum = Enddatum</strong>. von</label>
+            <div>
+                <label for="date[from]">
+                    Wann findet das statt? <span>Bei Ausstellungen: Startdatum = Enddatum</span>. von
+                </label>
                 <input name="data[date[from]]" type="text" id="date[from]" />
             </div>
-            <div class="input text">
+            <div>
                 <label for="date[till]">bis</label>
                 <input name="data[date[till]]" type="text" id="date[till]" />
             </div>
         </fieldset>
 
-        <section class="half2">
-            <h3 class="legend">Items in der Nähe</h3>
-            <ul id="nearbylist">
-                <li data-template="" class="nearbyitem">
-                    <a style="text-decoration:none; font-weight:normal" href="/items/edit/{{Item.parent}}">
+        <section class="nearby-items content-panel">
+            <h3>Items in der Nähe</h3>
+            <ul>
+                <li data-template="">
+                    <a href="/items/edit/{{Item.parent}}">
                         ({{Item.location.neighborhood|truncate 100}}) {{Item.title|truncate 80}}
                     </a>
                 </li>
@@ -157,47 +149,43 @@
     </form>
 </section>
 
-<!-- ######################## available import items pool ######################## -->
-<section class="panel1">
-    <h2 class="legend">Items</h2>
-    <ul id="itembox">
-        <li data-template="" class="newsitem">
-            <article>
-                <hgroup>
-                    <h3>{{title|truncate 45}}</h3>
-                </hgroup>
-                <p class="date">{{date.from}}</p>
-                <p>{{text|truncate 100}}</p>
-            </article>
-        </li>
-    </ul>
-</section>
+<section class="document-data">
+    <section class="item-pool content-panel">
+        <h2>Items</h2>
+        <ul>
+            <li data-template="">
+                <article>
+                    <hgroup>
+                        <h3>{{title|truncate 45}}</h3>
+                    </hgroup>
+                    <p>{{date.from}}</p>
+                    <p>{{text|truncate 100}}</p>
+                </article>
+            </li>
+        </ul>
+    </section>
 
-<!-- ######################## selected import item - meta data ######################## -->
-<section class="panel3">
-    <h2 class="legend">Meta Daten</h2>
-    <dl class="fieldsetInner">
-        <dt>Betreff</dt>
-        <dd></dd>
-        <dt>Von</dt>
-        <dd></dd>
-        <dt>Versanddatum</dt>
-        <dd>01:00 - 01.01.1970</dd>
-    </dl>
-</section>
+    <section class="item-meta-data content-panel">
+        <h2>Meta Daten</h2>
+        <dl>
+            <dt>Betreff</dt>
+            <dd></dd>
+            <dt>Von</dt>
+            <dd></dd>
+            <dt>Versanddatum</dt>
+            <dd>01:00 - 01.01.1970</dd>
+        </dl>
+    </section>
 
-<!-- ######################## selected import item - content data ######################## -->
-<section class="panel3">
-    <h2 class="legend">Content</h2>
-    <ul id="tabs">
-        <li>
-            <a href="#tabs-1">Inhalt</a>
-        </li>
-    </ul>
-    <div id="tabs-1">
-        <textarea id="textinner" readonly="readonly" class="textinner"></textarea>
-    </div>
+    <section class="item-content content-panel">
+        <h2>Content</h2>
+        <ul>
+            <li>
+                <a href="#content-tab-1">Inhalt</a>
+            </li>
+        </ul>
+        <div id="content-tab-1">
+            <textarea id="textinner" readonly="readonly" class="textinner"></textarea>
+        </div>
+    </section>
 </section>
-
-<!-- ######################## document footer ######################## -->
-<footer id="footer"></footer>
