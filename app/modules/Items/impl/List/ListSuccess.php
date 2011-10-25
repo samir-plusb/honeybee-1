05270a73282d777f7d5dbdce2eaff6ee8c4e0c09
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-10-25 10:21:42 +0000

    Reverted the strip_tags execution on our content item content. This kind of processing has to be done during import. refs #4111

--- app/modules/Items/impl/List/ListSuccess.php
+++ app/modules/Items/impl/List/ListSuccess.php
@@ -9,7 +9,7 @@ foreach ($t['items'] as $item)
     <li>
         <h3><?php echo $item['title']; ?></h3>
         <p>
-            <?php echo strip_tags($item['content']); ?>
+            <?php echo $item['content']; ?>
         </p>
     </li>
 
