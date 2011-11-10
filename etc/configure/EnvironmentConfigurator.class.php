265a22fe5689649d1b36e736b5d5962a2168e26c
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2011-11-10 10:58:55 +0000

    When setting up the project environment the required paths to get our node libs up and running are no set. refs #4591

--- etc/configure/EnvironmentConfigurator.class.php
+++ etc/configure/EnvironmentConfigurator.class.php
@@ -204,6 +204,18 @@ PHP_CODE;
         return <<<SH_CODE
 #!/bin/bash
 export PHP_COMMAND=%s
+
+# Project base path
+cw_path="`dirname $0`/.."
+cw_path="`readlink -f \${cw_path}`"
+                       
+# Nodejs libraries:
+export PATH="\${cw_path}/libs/node_modules/vows/bin:\$PATH"
+# - less-compile devtool
+export NODE_PATH="\${cw_path}/dev/less-compile/node_modules/less/lib:\$NODE_PATH"
+# - clientside test foundation
+export NODE_PATH=\${cw_path}/libs/node_modules/vows/lib:\$NODE_PATH
+export NODE_PATH=\${cw_path}/libs/node_modules/zombie/lib:\$NODE_PATH
 SH_CODE;
     }
 
