53a30c3197dd6c282711079c7bb727a53a776641
aeytom <tom.anheyer@berlinonline.de>
2011-12-02 10:30:27 +0000

    enritch project index page; refs #4801

--- app/modules/Default/impl/Index/IndexSuccess.php
+++ app/modules/Default/impl/Index/IndexSuccess.php
@@ -1,1 +1,24 @@
-<p>This is the ContentWorker's default index page.</p>
+<div class="topbar" data-scrollspy="scrollspy">
+	<div class="topbar-inner">
+		<div class="container-fluid">
+			<h2 class="left">
+				<a class="brand" href="<?php echo $ro->getBaseHref() ?>">Midas - News Stream</a>
+			</h2>
+		</div>
+	</div>
+</div>
+
+<div class="container" style="margin-top:40px">
+	<div class="content">
+		<div class="page-header">
+			<h1><?php echo $tm->_('Welcome to Midas','default.ui') ?></h1>
+		</div>
+		<div class="row">
+			<div class="span8">
+			    <ul>
+			        <li><a href="items/list/"><?php echo $tm->_('News item list','default.ui')?></a></li>
+			    </ul>
+			</div>
+		</div>
+	</div>
+</div>
