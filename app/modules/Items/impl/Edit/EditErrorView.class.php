36888ca774f9df6bdbdbd0ade3c3baa7abd5857a
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-01-17 16:37:21 +0000

    Added an error view for the edit item action. refs #5133

---
+++ app/modules/Items/impl/Edit/EditErrorView.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The Items_Edit_EditErrorView class handles Items/Edit write error data presentation.
+ *
+ * @version         $Id: $
+ * @copyright       BerlinOnline Stadtportal GmbH & Co. KG
+ * @author          Thorsten Schmitt-Rink <tschmittrink@gmail.com>
+ * @package         Items
+ * @subpackage      Mvc
+ */
+class Items_Edit_EditErrorView extends ItemsBaseView
+{
+    /**
+     * Handle presentation logic for commandline interfaces.
+     *
+     * @param       AgaviRequestDataHolder $parameters
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @codingStandardsIgnoreStart
+     */
+    public function executeJson(AgaviRequestDataHolder $parameters) // @codingStandardsIgnoreEnd
+    {
+        $this->getResponse()->setContent(json_encode(array(
+            'state' => 'error',
+            'msg' => 'Failed to process data.'
+        )));
+    }
+}
+
+?>
