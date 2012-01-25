ea496ccd4078614e44afaa970e147f09f895914c
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-01-25 17:52:31 +0000

    Removed the stage specific rewrite. Commented out the rss datasource example. And reactivated the login action. refs #5217

--- app/modules/Auth/impl/Login/LoginAction.class.php
+++ app/modules/Auth/impl/Login/LoginAction.class.php
@@ -53,16 +53,6 @@ class Auth_LoginAction extends AuthBaseAction
      */
     public function executeWrite(AgaviParameterHolder $rd)
     {
-        $user = $this->getContext()->getUser();
-        $user->setAuthenticated(TRUE);
-        $attr = array(
-            'login' => 'tschmitt',
-            'name' => 'Thorsten Schmitt-Rink',
-            'email' => false,
-            'acl_role' => $user->mapExternalRoleToDomain('ldap_group', 'files')
-        );
-        $user->setAttributes($attr);
-        return 'Success';
         $this->checkLdapConfig();
 
         $username = $rd->getParameter("username");
