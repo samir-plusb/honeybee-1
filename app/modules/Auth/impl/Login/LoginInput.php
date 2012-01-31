d0cd25b52f00bc14fa5d0db870b8d37bc1f0a393
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-01-31 11:06:29 +0000

    Added some room between the inputs. refs #5327

--- app/modules/Auth/impl/Login/LoginInput.php
+++ app/modules/Auth/impl/Login/LoginInput.php
@@ -2,46 +2,47 @@
 	<div class="topbar-inner">
 		<div class="container-fluid">
 			<h2 class="left">
-				<a class="brand" href="<?php echo $ro->getBaseHref() ?>">Midas - News Stream</a>
+				<a class="brand" href="<?php echo $ro->gen('index'); ?>">Midas</a>
 			</h2>
 		</div>
 	</div>
 </div>
 
-<div class="container" style="margin-top:40px">
+<div class="container" style="margin-top: 10em; width: 40em;">
 	<div class="content">
-		<div class="page-header">
-			<h1><?php echo $tm->_('Login','auth.ui') ?></h1>
-		</div>
-		<form action="<?php echo $ro->gen(NULL) ?>" method="post">
-<?php if (! empty($t['error'])) : ?>
+        <?php
+    if (! empty($t['error']))
+    {
+?>
 			<div class="row">
 			    <div class=" error span4 offset4">
 			        <?php echo htmlspecialchars($t['error'])?>
 		        </div>
 			</div>
-<?php endif ?>
-			<div class="row">
-				<div class="span4">
+<?php
+    }
+?>
+		<form action="<?php echo $ro->gen(NULL) ?>" method="post">
+			<fieldset>
+                <legend>
+                    <?php echo $tm->_('Login', 'auth.ui') ?>
+                </legend>
+                <div class="clearfix">
 					<label for="username"><?php echo $tm->_('User','auth.ui') ?></label>
-				</div>
-				<div class="span4">
-					<input type="text" name="username"/>
-				</div>
-			</div>
-			<div class="row">
-				<div class="span4">
+                    <div class="input">
+                        <input type="text" name="username"/>
+                    </div>
+                </div>
+                <div class="clearfix">
 					<label for="password"><?php echo $tm->_('Password','auth.ui') ?></label>
-				</div>
-				<div class="span4">
-					<input type="password" name="password" />
-				</div>
-			</div>
-			<div class="row">
-				<div class="span4 offset4">
-					<button class="btn primary" type="submit"><?php echo $tm->_('Sign in','auth.ui') ?></button>
-				</div>
-			</div>
+                    <div class="input">
+                        <input type="password" name="password" />
+                    </div>
+                </div>
+                    <div class="well">
+                        <button class="btn primary" type="submit"><?php echo $tm->_('Sign in','auth.ui') ?></button>
+                    </div>
+			</fieldset>
 		</form>
 	</div>
 </div>
