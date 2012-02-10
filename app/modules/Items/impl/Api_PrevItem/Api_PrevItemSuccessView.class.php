c1fcdfc8646b7e01c7e79138a64892e4e4842455
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-02-10 17:44:36 +0000

    Factored two methods that were duplicated amoung the api_previtem and api_nextitem actions. refs #5327

--- app/modules/Items/impl/Api_PrevItem/Api_PrevItemSuccessView.class.php
+++ app/modules/Items/impl/Api_PrevItem/Api_PrevItemSuccessView.class.php
@@ -9,56 +9,9 @@
  * @package         Items
  * @subpackage      Mvc
  */
-class Items_Api_PrevItem_Api_PrevItemSuccessView extends ItemsBaseView
+class Items_Api_PrevItem_Api_PrevItemSuccessView extends BrowseItemsBaseView
 {
-    /**
-     * Handle presentation logic for json requests.
-     *
-     * @param       AgaviRequestDataHolder $parameters
-     *
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     * @codingStandardsIgnoreStart
-     */
-    public function executeJson(AgaviRequestDataHolder $parameters) // @codingStandardsIgnoreEnd
-    {
-        $item = $this->getAttribute('item');
-        $itemData = NULL;
-        $ticketData = NULL;
-        if ($item)
-        {
-            $itemData = $item->toArray();
-            $importData = &$itemData['importItem'];
-            $importData['assets'] = $this->prepareAssets($item->getImportItem());
-            $ticketData = $this->getAttribute('ticket')->toArray();
-        }
 
-        $this->getResponse()->setContent(
-            json_encode(
-                array('state' => 'ok', 'item' => $itemData, 'ticket' => $ticketData)
-            )
-        );
-    }
-
-    protected function prepareAssets(IImportItem $item)
-    {
-        $assetService = ProjectAssetService::getInstance();
-        $assets = array();
-        $ro = $this->getContext()->getRouting();
-        foreach ($item->getMedia() as $mediaId)
-        {
-            $asset = $assetService->get($mediaId);
-            $assetData = $asset->toArray();
-            $metaData = $assetData['metaData'];
-            $assetCaption = isset($metaData['caption']) ? htmlspecialchars($metaData['caption']) : 'No Title';
-            $assets[] = array(
-                'id' => $mediaId,
-                'url' => $ro->gen('asset.binary', array('aid' => $mediaId)),
-                'caption' => sprintf('%s (%s)', $asset->getFullName(), $assetCaption),
-                'altText' => isset($metaData['alt']) ? htmlspecialchars($metaData['alt']) : $assetCaption
-            );
-        }
-        return $assets;
-    }
 }
 
 ?>
