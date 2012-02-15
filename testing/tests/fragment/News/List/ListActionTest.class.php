73d01b5d855e76b65c9a2586e948242e0c007015
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-02-15 13:52:47 +0000

    Added first draft fragment test for the News module's list action. Since the elastica autoload handler was not registered inside the isolated environment a new TestMethod.tpl template for isolated tests was added and now may be used by overriding the getTemplate method inside your tests. @see the ListActionTest for an example usage. refs #5495

---
+++ testing/tests/fragment/News/List/ListActionTest.class.php
@@ -0,0 +1,57 @@
+<?php
+
+class ListActionTest extends AgaviActionTestCase
+{
+    // As these are run outside of the code coverage's scope, they allways will be marked as non-executed.
+    // @codeCoverageIgnoreStart
+
+    public function __construct($name = NULL, array $data = array(), $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->contextName = 'web';
+        $this->moduleName = 'News';
+        $this->actionName = 'List';
+    }
+
+    // @codeCoverageIgnoreEnd
+
+    public function testRunImportWithoutParams()
+    {
+        $this->runActionWithParameters('read', array());
+        $this->assertViewNameEquals('Success');
+    }
+
+    /**
+     * run this action
+     *
+     * @param string $method request method like 'write', 'read'
+     * @param array $arguments for the action
+     */
+    protected function runActionWithParameters($method, array $arguments)
+    {
+        $this->setRequestMethod($method);
+
+        $this->setArguments(
+            $this->createRequestDataHolder(
+                array(
+                    AgaviConsoleRequestDataHolder::SOURCE_FILES => $arguments
+                )
+            )
+        );
+
+        $this->runAction();
+    }
+
+    protected function getTemplateFile()
+	{
+		if($this->doBootstrap())
+        {
+			return AgaviConfig::get('core.testing_dir') . DIRECTORY_SEPARATOR . 'TestCaseMethod.tpl';
+		}
+
+		return null;
+	}
+}
+
+?>
