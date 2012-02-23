d8a19b18c53872552020b4ceb8fab53ce46124e3
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2012-02-23 16:08:24 +0000

    The imperia data source and -import tests now pick a random number of documents from a given imperia category overview document and use these docids for testing. this gives us more variance and should not break due to records being removed after time. fixes #5617

--- testing/tests/unit/modules/Import/lib/imperia/dataimport/ImperiaImportTest.php
+++ testing/tests/unit/modules/Import/lib/imperia/dataimport/ImperiaImportTest.php
@@ -2,11 +2,7 @@
 
 class ImperiaImportTest extends IDataImportBaseTestCase
 {
-    static private $docIds = array( // normally these are provided by the imperia-trigger script.
-        '/2/10330/10343/10890/1389047',
-        '/2/10330/10343/10890/1385317',
-        '/2/10/65/368/1388875'
-    );
+    protected $docCount;
 
     protected function getImportName()
     {
@@ -23,8 +19,49 @@ class ImperiaImportTest extends IDataImportBaseTestCase
     protected function getDataSourceParameters($dataSourceName)
     {
         return array(
-            ImperiaDataSourceConfig::CFG_DOCIDS => self::$docIds
+            ImperiaDataSourceConfig::CFG_DOCIDS => $this->fetchRandomImperiaDocIds()
         );
+    }
+
+    protected function fetchRandomImperiaDocIds()
+    {
+        $this->docCount = rand(2, 10);
+
+        $imperiaUrl = AgaviConfig::get('import.imperia_cat_url');
+        $params = array();
+        parse_str(parse_url($imperiaUrl, PHP_URL_QUERY), $params);
+        if (! isset($params['catid']))
+        {
+            throw new PHPUnit_Framework_Exception("Unable to parse imperia category id from the given url.");
+        }
+
+        $curlHandle = ProjectCurl::create();
+        curl_setopt($curlHandle, CURLOPT_URL, $imperiaUrl);
+        $resp = curl_exec($curlHandle);
+        $err = curl_error($curlHandle);
+        $respCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
+        if ($respCode !== 200 || $err)
+        {
+            throw new PHPUnit_Framework_Exception("Unable to download imperia test documents");
+        }
+
+        $matches = array();
+        $pattern = sprintf('#<node_id>(%s\d+)</node_id>#is', $params['catid']);
+        if (! preg_match_all($pattern, $resp, $matches, PREG_SET_ORDER))
+        {
+            throw new PHPUnit_Framework_Exception("No suitable test docs found inside imperia category.");
+        }
+        $docsRead = 0;
+        foreach ($matches as $match)
+        {
+            if ($this->docCount === $docsRead)
+            {
+                break;
+            }
+            $docIds[] = $match[1];
+            $docsRead++;
+        }
+        return $docIds;
     }
 
     // @codeCoverageIgnoreEnd
--- testing/tests/unit/modules/Import/lib/imperia/datasource/ImperiaDataSourceTest.php
+++ testing/tests/unit/modules/Import/lib/imperia/datasource/ImperiaDataSourceTest.php
@@ -4,11 +4,7 @@ class ImperiaDataSourceTest extends DataSourceBaseTestCase
 {
     const CFG_CONFIG_FIXTURE = 'import/config/config.datasource.php';
 
-    static private $docIds = array( // normally these are provided by the imperia import trigger
-        '/2/10330/10343/10890/1389367',    // without media !!!
-        '/2/10330/10343/10890/1385317',
-        '/2/10/65/368/1388875'
-    );
+    protected $docCount;
 
     protected function getDataSourceClass()
     {
@@ -22,7 +18,7 @@ class ImperiaDataSourceTest extends DataSourceBaseTestCase
 
     protected function getExpectedLoopCount()
     {
-        return 3;
+        return $this->docCount;
     }
 
     protected function getExpectedRecordType()
@@ -33,13 +29,54 @@ class ImperiaDataSourceTest extends DataSourceBaseTestCase
     protected function getDataSourceParameters()
     {
         return array(
-            ImperiaDataSourceConfig::CFG_DOCIDS => self::$docIds
+            ImperiaDataSourceConfig::CFG_DOCIDS => $this->fetchRandomImperiaDocIds()
         );
     }
 
     protected function getDataSourceDescription()
     {
         return 'Provides imperia export xml data.';
+    }
+
+    protected function fetchRandomImperiaDocIds()
+    {
+        $this->docCount = rand(2, 10);
+
+        $imperiaUrl = AgaviConfig::get('import.imperia_cat_url');
+        $params = array();
+        parse_str(parse_url($imperiaUrl, PHP_URL_QUERY), $params);
+        if (! isset($params['catid']))
+        {
+            throw new PHPUnit_Framework_Exception("Unable to parse imperia category id from the given url.");
+        }
+
+        $curlHandle = ProjectCurl::create();
+        curl_setopt($curlHandle, CURLOPT_URL, $imperiaUrl);
+        $resp = curl_exec($curlHandle);
+        $err = curl_error($curlHandle);
+        $respCode = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
+        if ($respCode !== 200 || $err)
+        {
+            throw new PHPUnit_Framework_Exception("Unable to download imperia test documents");
+        }
+
+        $matches = array();
+        $pattern = sprintf('#<node_id>(%s\d+)</node_id>#is', $params['catid']);
+        if (! preg_match_all($pattern, $resp, $matches, PREG_SET_ORDER))
+        {
+            throw new PHPUnit_Framework_Exception("No suitable test docs found inside imperia category.");
+        }
+        $docsRead = 0;
+        foreach ($matches as $match)
+        {
+            if ($this->docCount === $docsRead)
+            {
+                break;
+            }
+            $docIds[] = $match[1];
+            $docsRead++;
+        }
+        return $docIds;
     }
 }
 
