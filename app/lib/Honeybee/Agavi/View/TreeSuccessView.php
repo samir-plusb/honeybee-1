5254d3ac174f10760f9f61965d5ba4706fb52638
Leon Weidauer <leon.weidauer@googlemail.com>
2013-02-01 20:10:52 +0100

    Added breadcrumbs to tree view

--- app/lib/Honeybee/Agavi/View/TreeSuccessView.php
+++ app/lib/Honeybee/Agavi/View/TreeSuccessView.php
@@ -18,6 +18,33 @@ class TreeSuccessView extends BaseView
                 'config' => $this->getAttribute('config'),
             ), NULL, 'read')
         );
+
+        $this->setBreadcrumb();
+    }
+
+    protected function setBreadcrumb()
+    {
+        $module = $this->getAttribute('module');
+
+        $treeRouteName = sprintf('%s.list', $module->getOption('prefix'));
+        $routing = $this->getContext()->getRouting();
+
+        $moduleCrumb = array(
+            'text' => $module->getName(),
+            'link' => $routing->gen($treeRouteName),
+            'info' => sprintf('%s - Baumansicht', $module->getName()),
+            'icon' => 'icon-list'
+        );
+
+        $breadcrumbs = $this->getContext()->getUser()->getAttribute('breadcrumbs', 'honeybee.breadcrumbs', array());
+        if (1 <= count($breadcrumbs))
+        {
+            array_splice($breadcrumbs, 1);
+        }
+
+        $this->getContext()->getUser()->setAttribute('modulecrumb', $moduleCrumb, 'honeybee.breadcrumbs');
+        $this->getContext()->getUser()->setAttribute('breadcrumbs', $breadcrumbs, 'honeybee.breadcrumbs');
+        
     }
 
     public function executeJson(\AgaviRequestDataHolder $requestData)
