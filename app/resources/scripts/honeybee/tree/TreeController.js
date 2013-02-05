3cf98570d45a9d7d9a2d1fb04f5e18858b4b6956
Leon Weidauer <leon.weidauer@googlemail.com>
2013-02-05 18:05:14 +0100

    Successful load/save roundtrip for tree structure

--- app/resources/scripts/honeybee/tree/TreeController.js
+++ app/resources/scripts/honeybee/tree/TreeController.js
@@ -15,7 +15,8 @@ honeybee.tree.TreeController = honeybee.core.BaseObject.extend({
 
         this.loadData();
 
-        this.renderTreeNode(this.tree.rootNode, this.renderTarget)
+        this.renderTreeNode(this.tree.rootNode, this.renderTarget);
+        this.refreshCss(this.renderTarget);
         this.bindDragEvents();
     },
 
@@ -31,7 +32,6 @@ honeybee.tree.TreeController = honeybee.core.BaseObject.extend({
         {
             ev.stopPropagation();
             ev.originalEvent.dataTransfer.setData('Text', $(this).attr('id'));
-            console.log('drag start');
         }).bind('dragover', function(ev)
         {
             ev.preventDefault();
@@ -41,8 +41,6 @@ honeybee.tree.TreeController = honeybee.core.BaseObject.extend({
             ev.preventDefault();
             that.moveNode($('#'+ev.originalEvent.dataTransfer.getData('Text')), $(this));
             ev.stopPropagation();
-
-            console.log('dropped', ev.originalEvent.dataTransfer.getData('Text'), 'on', $(this).attr('id'));
         });
     },
 
@@ -50,7 +48,10 @@ honeybee.tree.TreeController = honeybee.core.BaseObject.extend({
     {
         var childList, childContent, i;
 
+        domContext.attr('id', node.identifier);
         domContext.append($('<span></span>').addClass('node-label').text(node.label));
+
+        console.log('rendering', node.identifier);
 
         if (node.hasOwnProperty('children') && node.children.length > 0)
         {
@@ -61,7 +62,7 @@ honeybee.tree.TreeController = honeybee.core.BaseObject.extend({
             {
                 if (node.children.hasOwnProperty(i))
                 {
-                    childElement = $('<li></li>').addClass('child').attr('id', node.children[i].identifier).attr('draggable', true);
+                    var childElement = $('<li></li>').addClass('child').attr('draggable', true);
                     this.renderTreeNode(node.children[i], childElement);
 
                     childList.append(childElement);
@@ -70,29 +71,88 @@ honeybee.tree.TreeController = honeybee.core.BaseObject.extend({
         }
     },
 
+    refreshCss: function()
+    {
+        var even = false;
+        var depth = 0;
+        //encapsule in a function to avoid bloating mamory usage with "var that = this;" on every recursion
+        var traverseAndRefresh = function(domContext)
+        {
+            even = !even;
+            domContext.removeClass('odd even').addClass(even ? 'even' : 'odd');
+            domContext.children('.node-label').css('padding-left', depth*30);
+            depth = depth + 1;
+            domContext.children('ul').children('li').each(function(i, element)
+            {
+                traverseAndRefresh($(element));
+            });
+            depth = depth - 1;
+        };
+
+        traverseAndRefresh(this.renderTarget);
+    },
+
     loadData: function()
     {
         var dataContainer = this.domElement.find('.tree-data-json');
+
         this.tree = JSON.parse(dataContainer.text().trim());
+
+        console.log('loaded', JSON.stringify(this.tree));
     },
 
     moveNode: function(from, to)
     {
-        var children = to.children('.children');
-        if (children.length == 0)
+        if ($.contains(from, to))
         {
-            console.log('create children ul');
+            //we can't move a node into one of its children. that would probably destroy the fabric of space-time
+            return;
+        }
+        var children = to.children('.children');
+        if (children.length === 0)
+        {
             children = $('<ul></ul>').addClass('children');
             to.append(children);
         }
 
-        console.log('moving', from, to);
         children.append(from);
+
+        this.refreshCss();
+
+        this.rebuildJson();
+    },
+
+    rebuildJson: function()
+    {
+        var buildJsonNode = function(domContext)
+        {
+            var parent = domContext.parent();
+            var children = [];
+            domContext.children('.children').children('.child').each(function(i, element)
+            {
+                children.push(buildJsonNode($(element)));
+            });
+            var node = {
+                identifier: domContext.attr('id'),
+                label: domContext.children('.node-label').text(),
+                parent: parent.hasClass('children') ? parent.parent().attr('id') : null,
+                children: children
+            };
+
+            return node;
+        };
+
+        this.tree = {
+            rootNode: buildJsonNode(this.renderTarget)
+        };
+
+        this.saveData();
     },
 
     saveData: function()
     {
         this.tree.name = 'tree';
+        console.log('saving', JSON.stringify(this.tree));
         $.ajax({
             url: this.options.saveCompleteTreeUrl,
             type: 'POST',
@@ -108,7 +168,7 @@ honeybee.tree.TreeController = honeybee.core.BaseObject.extend({
 
         }).fail(function(response)
         {
-            console.log(response.responseText);
+            //console.log(response.responseText);
         });
     }
 });
