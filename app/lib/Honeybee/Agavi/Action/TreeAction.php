e28322991abc71911775d7c097e15201655837f7
Leon Weidauer <leon.weidauer@googlemail.com>
2013-02-07 15:27:07 +0100

    Fixed wrong merge

--- app/lib/Honeybee/Agavi/Action/TreeAction.php
+++ app/lib/Honeybee/Agavi/Action/TreeAction.php
@@ -3,7 +3,6 @@
 namespace Honeybee\Agavi\Action;
 
 use Honeybee\Core\Dat0r\Tree;
-use TreeConfig;
 
 class TreeAction extends BaseAction
 {
@@ -14,7 +13,6 @@ class TreeAction extends BaseAction
 
         $this->setAttribute('module', $module);
         $this->setAttribute('tree', $tree);
-        $this->setAttribute('config', TreeConfig::create($this->buildTreeConfig()));
 
         return 'Success';
     }
@@ -22,8 +20,6 @@ class TreeAction extends BaseAction
     public function executeWrite(\AgaviRequestDataHolder $requestData)
     {
         $treeData = $requestData->getParameter('structure');
-        $module = $this->getModule();
-        $this->setAttribute('module', $module);
         // to see the expected structure: $this->getModule()->getService()->getTree()->toArray()
         $tree = new Tree\Tree($this->getModule(), $treeData['name']);
         $tree->hydrate($treeData);
@@ -34,38 +30,5 @@ class TreeAction extends BaseAction
 
         return 'Success';
     }
-
-    protected function buildTreeConfig()
-    {
-        $settingsKey = $this->buildTreeConfigKey();
-        $treeSettings = \AgaviConfig::get($settingsKey, array());
-        $fields = array_values($this->getModule()->getFields()->toArray());
-
-        if (! isset($treeSettings['fields']))
-        {
-            $listFields = array();
-
-            for($i = 0; $i < 5 && $i < count($fields); $i++)
-            {
-                $field = $fields[$i];
-                $listFields[$field->getName()] = array(
-                    'name' => $field->getName(),
-                    'valuefield' => $field->getName(),
-                    'sortfield' => sprintf('%s.raw', $field->getName())
-                );
-            }
-            $treeSettings['fields'] = $listFields;
-        }
-
-        return $treeSettings;
-    }
-
-    protected function buildTreeConfigKey()
-    {
-        return sprintf(
-            '%s.tree_config', 
-            $this->getModule()->getOption('prefix')
-        );
-    }
-
 }
+
