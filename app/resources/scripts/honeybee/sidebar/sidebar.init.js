c320dc51c1b00fb89b331b7b03d89aa54f707d1d
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-02-25 12:55:39 +0000

    the resource-filter now correctly generates relative resource urls. now only initializing the sidebar if it is there.

--- app/resources/scripts/honeybee/sidebar/sidebar.init.js
+++ app/resources/scripts/honeybee/sidebar/sidebar.init.js
@@ -1,7 +1,9 @@
 (function(namespace, $, undefined)
 {
-    namespace.SidebarController.create('.sidebar', honeybee.sidebar);
-    namespace.SidebarController.create('.sidebar-slot', honeybee.sidebar);
-    namespace.SidebarController.create('.sidebar-tree', honeybee.sidebar);
-
-})(honeybee.sidebar);
+    if (0 < $('.sidebar').length)
+    {
+        namespace.SidebarController.create('.sidebar', honeybee.sidebar);
+        namespace.SidebarController.create('.sidebar-slot', honeybee.sidebar);
+        namespace.SidebarController.create('.sidebar-tree', honeybee.sidebar);
+    }
+})(honeybee.sidebar, $);
