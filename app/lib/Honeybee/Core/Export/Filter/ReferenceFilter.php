c36c0ebbbe42967a8f8696a2d4d87e6645cee0b5
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-04-03 18:08:34 +0200

    ReferenceFilter now resolves asset fields.
    When using the reference filter for exports it is now possible to refer to 'duck-typed' binary fields which will be resolved to a set of meta data such as size and mime-type for example. The latter duck-typing is achieved by setting an 'acts_as_assets_field' option on the integer-collection fields that are used to hold asset ids.

--- app/lib/Honeybee/Core/Export/Filter/ReferenceFilter.php
+++ app/lib/Honeybee/Core/Export/Filter/ReferenceFilter.php
@@ -3,6 +3,7 @@
 namespace Honeybee\Core\Export\Filter;
 
 use Honeybee\Core\Dat0r\Document;
+use Dat0r\Core\Runtime\Field;
 
 class ReferenceFilter extends BaseFilter
 {
@@ -27,12 +28,34 @@ class ReferenceFilter extends BaseFilter
                     {
                         $refData[$exportKey] = $refDocument->getShortIdentifier();
                     }
-                    else
+                    else 
                     {
-                        $refData[$exportKey] = $refDocument->getValue($fieldname);
+                        $field = $refDocument->getModule()->getField($fieldname);
+                        $actsAsAssetsField = (bool)$field->getOption('acts_as_assets_field', FALSE);
+
+                        if (TRUE === $actsAsAssetsField)
+                        {
+                            $assetsData = array();
+                            foreach ($refDocument->getValue($fieldname) as $assetId)
+                            {
+                                $asset = \ProjectAssetService::getInstance()->get($assetId);
+                                $assetsData[] = array(
+                                    'size' => $asset->getSize(),
+                                    'mime' => $asset->getMimeType(),
+                                    'filename' => $asset->getFullName(),
+                                    'copyright' => isset($metaData['copyright']) ? $metaData['copyright'] : '',
+                                    'copyrightUrl' => isset($metaData['copyright_url']) ? $metaData['copyright_url'] : '',
+                                    'caption' => isset($metaData['caption']) ? $metaData['caption'] : ''
+                                );
+                            }
+                            $refData[$exportKey] = $assetsData;
+                        }
+                        else
+                        {
+                            $refData[$exportKey] = $refDocument->getValue($fieldname);
+                        }
                     }
                 }
-
                 $filterOutput[$reference][] = $refData;
             }
         }
