8b6e9d9f56ce4ebcec9b6d4b9530f360065c4a09
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-06-10 14:03:25 +0200

    fixed indent for wd-reset mail.

--- app/modules/User/lib/UserService.class.php
+++ app/modules/User/lib/UserService.class.php
@@ -49,20 +49,20 @@ class UserService extends DocumentService
             array('token' => $user->getAuthToken())
         );
 
-        $message = sprintf(
-            'Hallo,
-            Sie erhalten diese Nachricht, weil im Content Management System des Projekts "%s" ein neues Passwort für Sie angefordert wurde.
-            Um sich neues Passwort zu erstellen, klicken Sie bitte auf den unten stehenden Link. Auf der Internetseite, die sich hinter diesem Link verbirgt, haben Sie die Möglichkeit, das neue Passwort zu hinterlegen.
+        $message = sprintf('
+Hallo,
+Sie erhalten diese Nachricht, weil im Content Management System des Projekts "%s" ein neues Passwort für Sie angefordert wurde.
+Um sich neues Passwort zu erstellen, klicken Sie bitte auf den unten stehenden Link. Auf der Internetseite, die sich hinter diesem Link verbirgt, haben Sie die Möglichkeit, das neue Passwort zu hinterlegen.
             
-            %s
+%s
             
-            Aus Sicherheitsgründen wir dieser Link nach 20 Minuten inaktiv. Danach können Sie sich diese E-Mail noch einmal zuschicken lassen.
-            Wichtig für Sie: Auch wenn Ihr Passwort verschlüsselt abgespeichert wird, sollten Sie darauf achten, stets schwer zu erratende Passwörter zu verwenden. 
-            Benutzen Sie beispielsweise niemals auf zwei Internetseiten dasselbe Passwort. 
-            Achten Sie außerdem darauf, möglichst eine Kombination aus Groß- und Kleinbuchstaben sowie mindestens eine Zahl zu verwenden.
-            Wenn Sie der Auffassung sind, dass Ihnen diese E-Mail irrtümlich zugeschickt wurde, wenden Sie sich bitte an %s (%s).
+Aus Sicherheitsgründen wir dieser Link nach 20 Minuten inaktiv. Danach können Sie sich diese E-Mail noch einmal zuschicken lassen.
+Wichtig für Sie: Auch wenn Ihr Passwort verschlüsselt abgespeichert wird, sollten Sie darauf achten, stets schwer zu erratende Passwörter zu verwenden. 
+Benutzen Sie beispielsweise niemals auf zwei Internetseiten dasselbe Passwort. 
+Achten Sie außerdem darauf, möglichst eine Kombination aus Groß- und Kleinbuchstaben sowie mindestens eine Zahl zu verwenden.
+Wenn Sie der Auffassung sind, dass Ihnen diese E-Mail irrtümlich zugeschickt wurde, wenden Sie sich bitte an %s (%s).
 
-            Diese E-Mail wurde automatisch erstellt.',
+Diese E-Mail wurde automatisch erstellt.',
             $project_name,
             $set_password_link,
             $project_contact['name'],
