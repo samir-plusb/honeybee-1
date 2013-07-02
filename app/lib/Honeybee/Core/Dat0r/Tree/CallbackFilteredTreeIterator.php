dd4363ebf6e129f17c04fc3c988b87d5f95f4319
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-07-02 14:33:05 +0200

    the filtered tree iterator is now php5.3 compatible.

--- app/lib/Honeybee/Core/Dat0r/Tree/CallbackFilteredTreeIterator.php
+++ app/lib/Honeybee/Core/Dat0r/Tree/CallbackFilteredTreeIterator.php
@@ -2,17 +2,20 @@
 
 namespace Honeybee\Core\Dat0r\Tree;
 
-class CallbackFilteredTreeIterator extends \CallbackFilterIterator
+class CallbackFilteredTreeIterator extends \FilterIterator
 {
     protected $tree;
 
     protected $ignore_level = -1;
 
-    public function __construct(ITree $tree, $callback)
+    protected $callback;
+
+    public function __construct(ITree $tree, \Closure $callback)
     {
         $this->tree = $tree;
+        $this->callback = $callback;
 
-        parent::__construct($this->tree->getIterator(), $callback);
+        parent::__construct($this->tree->getIterator());
     }
 
     public function accept()
@@ -24,7 +27,14 @@ class CallbackFilteredTreeIterator extends \CallbackFilterIterator
             return false;
         }
 
-        if (! parent::accept())
+        $externallyAccepted = call_user_func(
+            $this->callback,
+            $this->current(),
+            $this->key(),
+            $this->getInnerIterator()
+        );
+
+        if (! $externallyAccepted)
         {
             $this->ignore_level = $cur_level;
 
