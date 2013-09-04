4c156ef59d5204d268596c0ba810b29b0008899d
Steffen Gransow <graste@mivesto.de>
2013-09-04 10:38:24 +0200

    Views can now return closures for content
    This should enabled the returning of bigger XML content via XmlWriter etc.

---
+++ app/lib/Honeybee/Agavi/Response/WebResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Honeybee\Agavi\Response;
+
+/**
+ * WebResponse handles HTTP responses.
+ */
+class WebResponse extends \AgaviWebResponse
+{
+    /**
+     * It is now possible to return closures in views for streaming content etc.
+     *
+     * @example: return function() {
+     *     $writer = new XmlWriter();
+     *     $writer->openURI('php://output');
+     *     ...
+     *     $writer->flush();
+     * };
+     *
+     * @return void
+     */
+    public function sendContent()
+    {
+        if ($this->content instanceof \Closure) {
+            $callable = $this->content;
+            $callable();
+            return;
+        }
+
+        parent::sendContent();
+    }
+}
