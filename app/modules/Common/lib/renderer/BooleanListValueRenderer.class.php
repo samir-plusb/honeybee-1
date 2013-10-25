c7ce51b34c3fa223f7b5f3aa7dec6de2179c852d
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2013-10-25 21:56:07 +0200

    added support for the help component to checkbox fields. added a list renderer for boolean values.

---
+++ app/modules/Common/lib/renderer/BooleanListValueRenderer.class.php
@@ -0,0 +1,21 @@
+<?php
+
+use Honeybee\Core\Dat0r\Module;
+
+class BooleanListValueRenderer extends DefaultListValueRenderer
+{
+    protected $module;
+
+    public function __construct(Module $module)
+    {
+        $this->module = $module;
+    }
+
+    public function renderValue($value, $field, array $data = array())
+    {
+        $translation_domain = sprintf('%s.list', $this->module->getOption('prefix'));
+        $translation_domain = isset($options['domain']) ? $options['domain'] : $translation_domain;
+
+        return AgaviContext::getInstance()->getTranslationManager()->_($value === true ? 'yes' : 'no', $translation_domain);
+    }
+}
