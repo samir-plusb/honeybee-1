b3af6da9bdc4916a0cca35c33d847711bc391df2
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2014-02-06 18:16:01 +0100

    the reference widget now supports dragndrop sort

--- app/resources/scripts/honeybee/widgets/Reference.js
+++ app/resources/scripts/honeybee/widgets/Reference.js
@@ -89,6 +89,33 @@ honeybee.widgets.Reference = honeybee.widgets.Widget.extend({
             }),
             this.options.event_origin
         );
+
+        this.element.find('.tagslist-list').sortable({
+            onDrop: function  (item, targetContainer, _super) {
+                var clonedItem = $('<li/>').css({height: 3});
+                item.before(clonedItem);
+                clonedItem.animate({'height': item.height()});
+                item.animate(clonedItem.position(), function () {
+                    clonedItem.detach();
+                    _super(item);
+                });
+            },
+            onDragStart: function ($item, container, _super) {
+                var offset = $item.offset();
+                var pointer = container.rootGroup.pointer;
+                adjustment = {
+                    left: pointer.left - offset.left,
+                    top: pointer.top - offset.top
+                }
+                _super($item, container);
+            },
+            onDrag: function ($item, position) {
+                $item.css({
+                    left: position.left - adjustment.left,
+                    top: position.top - adjustment.top
+                });
+            }
+        });
     },
 
     buildPopover: function()
