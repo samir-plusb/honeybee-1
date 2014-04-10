3ce782f22e3cb572fb627c2dc5b2980fbff92b71
Steffen Gransow <graste@mivesto.de>
2014-04-10 13:43:52 +0200

    Many more translations plus save_and_close works
    now by going back to the last list view instead of the "create" view.
    
    The translations are mainly for the User module. The module templates now
    use translation domains on the generated validation xml files.

--- app/lib/Honeybee/Agavi/View/EditSuccessView.php
+++ app/lib/Honeybee/Agavi/View/EditSuccessView.php
@@ -17,14 +17,13 @@ class EditSuccessView extends BaseView
             'data' => $document->toArray()
         );
 
-        if ('save_and_new' === $requestData->getParameter('save_type'))
-        {
+        if ('save_and_new' === $requestData->getParameter('save_type')) {
             $data['redirect_url'] = $routing->gen(sprintf('%s.edit', $module_prefix));
         } elseif ('save_and_close' === $requestData->getParameter('save_type')) {
             $list_setting_name = sprintf('%s_last_list_url', $module_prefix);
             $last_list_url = $this->getContext()->getUser()->getAttribute($list_setting_name, 'honeybee.list', false);
             if (!$last_list_url) {
-                $last_list_url = $routing->gen(sprintf('%s.edit', $module_prefix));
+                $last_list_url = $routing->gen(sprintf('%s.list', $module_prefix));
             }
             $data['redirect_url'] = $last_list_url;
         }
